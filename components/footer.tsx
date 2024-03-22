d07ec0dd2bce71b280a91e464694c139193e51cb
Josh Maguire <jmaguire@pcs-publishing.com>
2024-03-22 16:30:24 +0000

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='mb-8 px-4 text-center text-gray-500'>
+      <small className='mb-2 block text-xs'>&copy; 2024 wordle-clone</small>
       <p className='text-xs'>
         Built by Josh. Uses React, TypeScript, Tailwind CSS, hosted using
         Vercel.
